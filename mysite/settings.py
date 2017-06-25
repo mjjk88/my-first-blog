69d453a1ed4e983d01a431cbe225249cddc1dd24
ja <monika.kaja@gmail.com>
2017-06-25 11:31:01 +0200

    Host adding

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'yv-epqnv6jj8!vbgeoi3-=6xv&19yh4v^-hhr8#_3coa2g^5f5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['monikakaja.pythonanywhere.com']
 
 
 # Application definition
